197a5d5627c1280292705e32063071a5f9243572
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-02 21:08:42 +0700

    change App style import font OpenSans remove spacing container set font using OpenSans add style backgroundColor, color

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,20 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+html,
+body {
+  font-family: 'Open Sans', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  background-color: $lightColor;
+  color: $grayColor;
+  background-color: $whiteColor;
 }
 </style>
