82f6ef3efffd76c4c5ddb7c0cbb810463c74d46b
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-02 23:12:30 +0700

    update App render component Navbar to App

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
+  <Navbar />
   <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Navbar from './components/Navbar.vue';
 
 export default defineComponent({
   name: 'App',
+  components: {
+    Navbar,
+  },
 });
 </script>
 
