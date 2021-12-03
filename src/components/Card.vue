df56b1705dfc3ff08a6fd75e7a420a418647fc15
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-03 19:50:46 +0700

    create new component Card

---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card">
+    <img class="card-image" :src="image" />
+    <div class="card-content">
+      <div class="card-title">{{ title }}</div>
+      <div class="card-description">{{ description }}</div>
+      <div class="card-time">{{ published_at }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Card',
+  props: {
+    image: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    published_at: {
+      type: String,
+    },
+  },
+});
+</script>
+
+<style lang="scss"></style>
