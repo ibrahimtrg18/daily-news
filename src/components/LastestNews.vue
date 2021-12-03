9232655365d5cf3d093cfc26e18bc5d8d48f901d
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-03 21:17:47 +0700

    create LastestNews component

---
+++ src/components/LastestNews.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="lastest-news">
+    <h1 class="title">Lastest News</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LastestNews',
+});
+</script>
+
+<style lang="scss">
+.lastest-news {
+  & > .title {
+    color: $primaryColor;
+    text-transform: capitalize;
+    font-size: 1.75rem;
+    font-weight: 800;
+    line-height: 120%;
+    text-align: center;
+    margin-bottom: 25px;
+  }
+}
+</style>
