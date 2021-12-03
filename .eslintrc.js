bbbbfb4e215fd16ae1dbac1d0e189069b6f8a65e
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-03 20:54:09 +0700

    add new rules turn off import/prefer-default-export allowi to inline export

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': 'off',
+    'import/prefer-default-export': 'off',
     '@typescript-eslint/no-var-requires': 'off',
   },
 };
