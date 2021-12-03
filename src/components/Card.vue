fc273372bddfbea673d379f4744549c6549402e3
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-04 00:30:04 +0700

    improving style, content, fix props data

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="card">
-    <img class="card-image" :src="image" />
+    <img class="card-image" :src="image" alt="title" v-if="image" />
     <div class="card-content">
-      <div class="card-title">{{ title }}</div>
-      <div class="card-description">{{ description }}</div>
-      <div class="card-time">{{ published_at }}</div>
+      <h1 class="card-title" v-html="title" />
+      <p class="card-description" v-html="description" />
+      <div class="card-footer">
+        <span class="card-source">{{ source }}</span>
+        <span class="card-line"></span>
+        <span class="card-time">{{ convertDateTimeFromNow(publishedAt) }}</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { convertDateTimeFromNow } from '../utils/convertDateTime';
 
 export default defineComponent({
   name: 'Card',
@@ -24,11 +29,79 @@ export default defineComponent({
     description: {
       type: String,
     },
-    published_at: {
+    publishedAt: {
       type: String,
     },
+    source: {
+      type: String,
+    },
+  },
+  methods: {
+    convertDateTimeFromNow,
   },
 });
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.card {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0 0 20px 10px $cardBoxShadowColor;
+
+  & > .card-image {
+    width: 100%;
+    height: auto;
+    max-height: 300px;
+    object-fit: cover;
+  }
+
+  & > .card-content {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    padding: 15px;
+
+    & > .card-title {
+      font-size: 1.25rem;
+      line-height: 110%;
+      color: $primaryColor;
+      font-weight: 600;
+      margin-bottom: 10px;
+
+      &::first-letter {
+        text-transform: capitalize;
+      }
+    }
+
+    & > .card-description {
+      font-size: 0.85rem;
+      line-height: 110%;
+      margin-bottom: 10px;
+    }
+
+    & > .card-footer {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 5px;
+      align-items: center;
+      font-size: 0.75rem;
+      line-height: 100%;
+      margin-top: auto;
+
+      & > .card-source {
+        color: $secondaryColor;
+      }
+
+      & > .card-line {
+        width: 1px;
+        height: 0.75rem;
+        background-color: $textColor;
+      }
+
+      & > .card-time {
+      }
+    }
+  }
+}
+</style>
