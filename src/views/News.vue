3acb0a2b06b7aff537500d0bca72633c6d69ff89
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-04 00:32:52 +0700

    show LastestNews

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,10 +1,18 @@
-<template>News</template>
+<template>
+  <div class="content">
+    <LastestNews />
+  </div>
+</template>
 
 <script>
 import { defineComponent } from 'vue';
+import LastestNews from '../components/LastestNews.vue';
 
 export default defineComponent({
   name: 'News',
+  components: {
+    LastestNews,
+  },
 });
 </script>
 
