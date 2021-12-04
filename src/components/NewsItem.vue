873b27ae5788a86a3d636ce2353741e921645600
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-04 18:27:49 +0700

    create component NewsItem

---
+++ src/components/NewsItem.vue
@@ -0,0 +1,101 @@
+<template>
+  <a class="news-item" :href="url" target="_blank">
+    <div class="news-image">
+      <img :src="image" :alt="name" />
+    </div>
+    <div class="news-content">
+      <h1 class="news-title" v-html="title" />
+      <p class="news-description" v-html="description" />
+    </div>
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NewsItem',
+  props: {
+    image: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    publishedAt: {
+      type: String,
+    },
+    source: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+.news-item {
+  display: flex;
+  align-items: flex-start;
+  max-width: 500px;
+  text-decoration: none;
+  color: $textColor;
+
+  &:hover {
+    background-color: #fff;
+    box-shadow: 0 0 20px 10px $cardBoxShadowColor;
+  }
+
+  & > .news-image {
+    display: block;
+    flex: 1 0 200px;
+    width: max-content;
+    height: 100%;
+
+    & > img {
+      height: 100%;
+      max-height: 350px;
+      object-fit: cover;
+    }
+  }
+
+  & > .news-content {
+    padding: 10px;
+
+    & > .news-title {
+      font-size: 1.15rem;
+      line-height: 110%;
+      color: $primaryColor;
+      font-weight: 600;
+      margin-bottom: 10px;
+
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+      text-overflow: ellipsis;
+      overflow: hidden;
+
+      &::first-letter {
+        text-transform: capitalize;
+      }
+    }
+
+    & > .news-description {
+      font-size: 0.75rem;
+      line-height: 110%;
+      margin-bottom: 10px;
+
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+  }
+}
+</style>
