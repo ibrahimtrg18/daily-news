6458256242a8cbbc1d8c41dfe95c7329971eb671
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-04 11:39:56 +0700

    setup i18n to app

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
 import App from './App.vue';
 import router from './router';
+import messages from './utils/messages';
 
-createApp(App).use(router).mount('#app');
+const i18n = createI18n({
+  locale: 'en',
+  messages,
+});
+
+createApp(App).use(router).use(i18n).mount('#app');
---
+++ src/utils/messages.ts
@@ -0,0 +1,14 @@
+export default {
+  id: {
+    message: {
+      menuNews: 'Berita',
+      menuNewsTitle: 'Berita Utama',
+    },
+  },
+  en: {
+    message: {
+      menuNews: 'News',
+      menuNewsTitle: 'Top Headlines',
+    },
+  },
+};
