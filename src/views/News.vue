dc9a595ade361d9e13cef2bd791ed4af158af5a1
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-04 17:08:12 +0700

    render FeatureNews to News

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="content">
     <LastestNews :language="language" />
+    <FeatureNews :language="language" />
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import LastestNews from '../components/LastestNews.vue';
+import FeatureNews from '../components/FeatureNews.vue';
 
 export default defineComponent({
   name: 'News',
   components: {
     LastestNews,
+    FeatureNews,
   },
   props: ['language'],
 });
