513ddf62f7ad981da1d3b9152290dc16567ac8b5
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-04 18:30:37 +0700

    improving UI UX FeatureNews fetching data articles add new style ffor news-list

--- src/components/FeatureNews.vue
+++ src/components/FeatureNews.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="feature-news">
     <div class="header">
-      <h1 class="title">Feature News</h1>
+      <h1 class="title">Yesterday</h1>
       <div class="action">
         <span class="icon left">
           <Icon name="arrow-left" />
@@ -11,18 +11,78 @@
         </span>
       </div>
     </div>
-    <div class="news-list"></div>
+    <div class="loading" v-if="isLoading">Loading...</div>
+    <div class="news-list" v-else-if="articles.length > 0">
+      <NewsItem
+        v-for="article in articles"
+        :key="article.title"
+        :title="article.title"
+        :description="article.description"
+        :image="article.urlToImage"
+        :publishedAt="article.publishedAt"
+        :source="article.source.name"
+        :url="article.url"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { fetchTopHeadlines } from '../utils/services';
+import NewsItem from './NewsItem.vue';
 import Icon from './Icon.vue';
+
+interface Article {
+  author?: string | null;
+  title?: string | null;
+  description?: string | null;
+  url?: string | null;
+  source?: string | null;
+  image?: string | null;
+  category?: string | null;
+  language?: string | null;
+  country?: string | null;
+  published_at?: string | null;
+}
 
 export default defineComponent({
   name: 'FeatureNews',
   components: {
+    NewsItem,
     Icon,
+  },
+  props: {
+    language: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      articles: [] as Article[],
+      isLoading: false,
+    };
+  },
+  methods: {
+    fetchTopHeadlines,
+  },
+  async mounted() {
+    this.isLoading = true;
+    const data = await fetchTopHeadlines({ country: this.language, limit: 10, page: 3 });
+
+    this.isLoading = false;
+    this.articles = data;
+  },
+  watch: {
+    async language(value) {
+      this.isLoading = true;
+      const data = await fetchTopHeadlines({
+        country: value,
+      });
+
+      this.isLoading = false;
+      this.articles = data;
+    },
   },
 });
 </script>
@@ -34,7 +94,8 @@ export default defineComponent({
     align-items: center;
     justify-content: space-between;
     border-bottom: 1px solid $borderColor;
-    padding-bottom: 5px;
+    padding-bottom: 10px;
+    margin-bottom: 15px;
 
     & > .title {
       @include title(left);
@@ -54,6 +115,13 @@ export default defineComponent({
         }
       }
     }
+  }
+
+  & > .news-list {
+    display: flex;
+    flex-wrap: nowrap;
+    gap: 10px;
+    overflow: hidden;
   }
 }
 </style>
