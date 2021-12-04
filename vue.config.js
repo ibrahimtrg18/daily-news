61c10a651b42e04250e12551d909214b79ea90dd
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-04 16:47:41 +0700

    create style mixins

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        additionalData: '@import "~@/assets/scss/_variables.scss";',
+        additionalData: `@import "~@/assets/scss/_variables.scss";
+          @import "~@/assets/scss/_mixins.scss";`,
       },
     },
   },
