d3cab6801458e799661174914e62dc5dd6df92bf
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-05 13:15:06 +0700

    add new icon chevron-left, chevron-right

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -67,6 +67,38 @@
     <line x1="5" y1="12" x2="19" y2="12"></line>
     <polyline points="12 5 19 12 12 19"></polyline>
   </svg>
+
+  <svg
+    v-if="name === 'chevron-left'"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-chevron-left"
+  >
+    <polyline points="15 18 9 12 15 6"></polyline>
+  </svg>
+
+  <svg
+    v-if="name === 'chevron-right'"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-chevron-right"
+  >
+    <polyline points="9 18 15 12 9 6"></polyline>
+  </svg>
 </template>
 
 <script>
