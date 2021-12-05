6cda349a5d6453cbbd138bcda6d7eb0cf5a9f47e
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-05 09:28:24 +0700

    create new component CategoryNews

---
+++ src/components/CategoryNews.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="category-news">
+    <h1 class="title">{{ title }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CategoryNews',
+  props: {
+    title: {
+      type: String,
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+.category-news {
+  & > .title {
+    @include title(1.5rem, left, 0);
+  }
+}
+</style>
