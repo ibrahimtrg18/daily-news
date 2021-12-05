fbb8dcca895eda877622c4c9e99aa63078094bca
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-05 09:26:44 +0700

    create view Politic add Politic view to route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import News from '../views/News.vue';
+import Politic from '../views/Politic.vue';
 
 const routes = [
   {
     path: '/',
     name: 'News',
     component: News,
+  },
+  {
+    path: '/politic',
+    name: 'Politic',
+    component: Politic,
   },
 ];
 
---
+++ src/views/Politic.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="content">Politic</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Politic',
+  components: {},
+  props: ['language'],
+});
+</script>
+
+<style lang="scss"></style>
