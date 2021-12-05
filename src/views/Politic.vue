3e544d8290eac12f495b780f2e4c67b96d069ad6
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-05 10:54:01 +0700

    improving UI UX render component CategoryNew fetch data articles using service fetchTopHeadlines by language

--- src/views/Politic.vue
+++ src/views/Politic.vue
@@ -1,14 +1,53 @@
 <template>
-  <div class="content">Politic</div>
+  <div class="content">
+    <CategoryNews :title="$t('politic.title')" :articles="articles"> </CategoryNews>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { Article } from '../interfaces/Articles';
+import CategoryNews from '../components/CategoryNews.vue';
+import { fetchTopHeadlines } from '../utils/services';
 
 export default defineComponent({
   name: 'Politic',
-  components: {},
+  components: {
+    CategoryNews,
+  },
   props: ['language'],
+  data() {
+    return {
+      articles: [] as Article[],
+      isLoading: false,
+    };
+  },
+  methods: {
+    async fetchTopHeadlinesByLanguage(language: string) {
+      try {
+        const data = await fetchTopHeadlines({
+          country: language,
+          limit: 10,
+          page: 1,
+          category: 'politics',
+        });
+        return data;
+      } catch (err) {
+        console.error(err);
+        return {
+          articles: [],
+        };
+      }
+    },
+  },
+  async mounted() {
+    this.isLoading = true;
+    const data = await this.fetchTopHeadlinesByLanguage(this.language!);
+    if (data.articles) {
+      this.articles = data.articles;
+    }
+    this.isLoading = false;
+  },
 });
 </script>
 
