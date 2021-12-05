71111a358ee7e7ffb16300c6db09cf9aacffcb9c
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-05 15:05:14 +0700

    create new view Search for search article news

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createWebHistory, createRouter } from 'vue-router';
 import News from '../views/News.vue';
 import Politic from '../views/Politic.vue';
 import Health from '../views/Health.vue';
+import Search from '../views/Search.vue';
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/health',
     name: 'Health',
     component: Health,
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: Search,
   },
 ];
 
---
+++ src/views/Search.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content">Search</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Article } from '../interfaces/Articles';
+
+export default defineComponent({
+  name: 'Search',
+  props: ['language'],
+  data() {
+    return {
+      articles: [] as Article[],
+      isLoading: false,
+      page: 1,
+    };
+  },
+});
+</script>
+
+<style lang="scss"></style>
