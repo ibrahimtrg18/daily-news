1ddcb939c23732eb05db73748592ce0e5d9ce30e
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-12-05 13:15:41 +0700

    create pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="pagination">
+    <div class="pagination-control">
+      <button class="pagination-action left">
+        <span class="icon chevron-left" @click="onPageChange(page - 1)">
+          <Icon name="chevron-left" />
+        </span>
+      </button>
+      <div class="pagination-number">
+        <span>{{ page }}</span>
+      </div>
+      <button class="pagination-action right">
+        <span class="icon chevron-right" @click="onPageChange(page + 1)">
+          <Icon name="chevron-right" />
+        </span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Icon from './Icon.vue';
+
+export default defineComponent({
+  name: 'Pagination',
+  components: {
+    Icon,
+  },
+  props: {
+    page: {
+      type: Number,
+    },
+  },
+  methods: {
+    onPageChange(newPageValue: number) {
+      this.$emit('update:page', newPageValue);
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+.pagination {
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+
+  & > .pagination-control {
+    display: flex;
+    user-select: none;
+
+    & > .pagination-number {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 0.25rem 1rem;
+      background-color: $backgroundColor;
+      line-height: 100%;
+      border: 1px solid $whiteColor;
+
+      & > span {
+        color: $primaryColor;
+      }
+    }
+
+    & > button.pagination-action {
+      background-color: $backgroundColor;
+      border: 1px solid $whiteColor;
+      padding: 0.25rem 0.5rem;
+      cursor: pointer;
+
+      &:hover {
+        background-color: $whiteColor;
+      }
+    }
+  }
+}
+</style>
